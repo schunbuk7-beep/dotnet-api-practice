97b8057daad15651da2c067de010042ef9d03c21
Win_ter7 <schunbuk7@gmail.com>
2026-03-18 12:09:26 +0000

    Day7: Added model validation for API requests

--- DotnetApiPractice/DTOs/StudentCreateDto.cs
+++ DotnetApiPractice/DTOs/StudentCreateDto.cs
@@ -1,9 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DotnetApiPractice.DTOs;
 
 public class StudentCreateDto
 {
+    [Required (ErrorMessage = "Name is Required")]
+    [MaxLength(100, ErrorMessage = "Name Cannot Exeed more than 100 characters")]
     public string Name { get; set; }
-        public int Age { get; set; }
-            public string Course { get; set; }
-                public string? Email { get; set; }
-                }
+
+    [Required (ErrorMessage = "Age is Required")]
+    [Range(1,150,ErrorMessage = "Age Must be between 1 to 150")]
+    public int Age { get; set; }
+
+    [Required(ErrorMessage = "Course is Required")]
+    [MaxLength(200, ErrorMessage = "Course cannot Exeed more than 200 characters")]
+    public string Course { get; set; }
+
+    [EmailAddress(ErrorMessage = "Please Provide a valid Email Address")]
+     public string? Email { get; set; }
+     
+ }
--- DotnetApiPractice/obj/Debug/net10.0/DotnetApiPractice.AssemblyInfo.cs
+++ DotnetApiPractice/obj/Debug/net10.0/DotnetApiPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetApiPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb29ac4a790e0e7ea996dd660c6e8d0ea72a500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf2f1b952f0377df4b7e2151a608d50633cd153")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetApiPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetApiPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
